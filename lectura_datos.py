cb2264fef8fc7f2ad2f907d9f4127610d3caa24b
thomatrix <tschade91@gmail.com>
2022-08-30 21:36:37 -0400

    Add files via upload

---
+++ lectura_datos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 14:12:21 2022
+
+@author: tscha
+"""
+
+import pandas as pd
+
+#%% Lectura de datos
+df_educacion=pd.read_excel("data/educacion.xlsx",header=3)
